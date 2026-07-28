c3a61dbdf55bf6ccb13109d7d76c0246af89008d
tttz <tanzhuanjiajieshao@gmail.com>
2026-07-28 11:40:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 @st.cache_resource
 def load_model():
     model = tf.keras.layers.TFSMLayer(
-        "model",
+        "model/tomato_model.keras",
         call_endpoint="serving_default"
     )
     return model
